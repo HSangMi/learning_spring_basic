8764170859dab6041321736c798d36eedc2f14d1
han32 <mii2425@naver.com>
2024-01-25 01:43:44 +0900

    2-3 AppConfig 리팩터링 2-4 새로운 할인정책으로 변경해보기 2-5 [정리] 객체지향설계5가지 원칙 적용

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,9 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -8,16 +11,45 @@ import hello.core.order.OrderService;
 import hello.core.order.OrderserviceImpl;
 
 /**
- * 어플리케이션의 전체 동작 방식을 구성(config)하기 위해, **구현 객체를 생성**하고 **연결**하는 책임을 가지는 별도의 설정클래스 만들기
+ * 1. 어플리케이션의 전체 동작 방식을 구성(config)하기 위해, **구현 객체를 생성**하고 **연결**하는 책임을 가지는 별도의 설정클래스 만들기
+ * 2. **역할**과 **구현**이 잘 드러나게 바꾸기!
  */
 public class AppConfig {
+    /**
+     * 2 AppConfig 리팩토링 전
+     * - 역할이 명확하지 않고 중복된 코드들이 있음
+     */
+//    public MemberService memberService(){
+//        // 생성자 주입
+//        return new MemberServiceImpl(new MemoryMemberRepository());
+//    }
+//
+//    public OrderService orderService(){
+//        return new OrderserviceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+//    }
+    /** FIXME 역할이 잘 드러나도록 변경! (단축키 ctrl + alt + M 2번 누르면 리턴타입 선택가능! 구체클래스x 인터페이스로 선택! )
+     - 장점
+        - 메소드 이름만 봐도 역할이 구분되고, 그 구현이 어떤건지 한눈에 들어옴 + 중복코드가 사라짐.
+        - 이후 구현체가 바뀌었을 때, 이 곳에서만 관리해주면 된다.
+     */
 
+    /* MemberService 역할 */
     public MemberService memberService(){
         // 생성자 주입
-        return new MemberServiceImpl(new MemoryMemberRepository());
+//        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
-
+    /* MemberRepository 역할 */
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+    /* OrderService 역할 */
     public OrderService orderService(){
-        return new OrderserviceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderserviceImpl(memberRepository(), discountPolicy());
+    }
+    /* DicountPolicy 역할 */
+    public DiscountPolicy discountPolicy(){
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
