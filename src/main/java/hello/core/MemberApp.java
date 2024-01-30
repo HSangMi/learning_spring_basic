5f11cb7314ee357239dae382295fbebd7d18256e
han32 <mii2425@naver.com>
2024-01-31 03:10:50 +0900

    2-7 스프링으로 전환해보기

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,16 +4,28 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     // psvm
     // 순수 자바로만 실행되는 테스트 로직 => Junit으로 테스트(test 패키지에서 진행)
     public static void main(String[] args) {
+/** 스프링으로 전환하기
         // appConfig에서 설정한(주입해서 만든) memberService 꺼내옴
         AppConfig appConfig = new AppConfig();
+        // MemberService memberService = new MemberServiceImpl();
         MemberService memberService = appConfig.memberService();
-//        MemberService memberService = new MemberServiceImpl();
+*/
+        //ApplicationContext : Spring 의 시작점! 스프링의 컨테이너!
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+                                                // 어노테이션 기반으로 Config하겠다. 파라미터에 설정파일.class 전달!
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+                                                // default: bean등록한 method명이 name으로 사용됨, 타입 class 전달
+
+
+
         Member memberA = new Member(1L, "memberA", Grade.VIP);
         memberService.join(memberA);
 
--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -7,16 +7,26 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderserviceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     // order 서비스가 제대로 동작하는지 확인해보자! (Junit 익숙해지기 전)
     public static void main(String[] args) {
+        
+/** 스프링으로 전환하기
         AppConfig appConfig = new AppConfig();
         MemberService memberService = appConfig.memberService();
         OrderService orderService = appConfig.orderService();
 //        MemberService memberService = new MemberServiceImpl();
 //        OrderService orderService = new OrderserviceImpl();
+*/
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
+
+
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
