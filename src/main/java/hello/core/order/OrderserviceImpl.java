164793cd2c59b4274f0224d38d9ab4c6598ac476
HAN32-NOTEBOOK\han32 <mii2425@naver.com>
2024-07-09 19:20:01 +0900

    6-4-1 의존관계 자동주입 - 조회 빈이 2개 이상일 경우 - 1. @Autowired 필드명 매칭

--- src/main/java/hello/core/order/OrderserviceImpl.java
+++ src/main/java/hello/core/order/OrderserviceImpl.java
@@ -11,18 +11,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class OrderserviceImpl implements OrderService{
 
     //    추상클래스(인터페이스)에만 의존하도록 변경
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy; // final 선언되어있으면 기본할당이든 생성자인든 할당이되어야함!
 
-//    @Autowired
-//    public OrderserviceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-//    this.memberRepository = memberRepository;
-//        this.discountPolicy = discountPolicy;
-//    }
+    // Autowired 주입 시, 빈이 2개 이상인 경우, 필드이름으로 매칭 (rateDiscountPolicy)
+    // (참고, appConfig.xml에 discountPolicy이름으로 bean이 등록되어있어서 전체테스트코드는 실패)
+    @Autowired
+    public OrderserviceImpl(MemberRepository memberRepository, DiscountPolicy rateDiscountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = rateDiscountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
@@ -38,4 +40,6 @@ public class OrderserviceImpl implements OrderService{
     public MemberRepository getMemberRepository(){
         return memberRepository;
     }
+
+    public DiscountPolicy getDiscountPolicy(){ return discountPolicy; }
 }
--- src/test/java/hello/core/scan/AutoAppConfigTest.java
+++ src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -1,7 +1,10 @@
 package hello.core.scan;
 
 import hello.core.AutoAppConfig;
+import hello.core.discount.DiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
+import hello.core.order.OrderserviceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
@@ -15,5 +18,11 @@ public class AutoAppConfigTest {
 
         MemberService memberService = ac.getBean(MemberService.class);
         Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+
+        OrderserviceImpl bean = ac.getBean(OrderserviceImpl.class);
+        MemberRepository memberRepository = bean.getMemberRepository();
+        System.out.println("memberRepository : " + memberRepository);
+        DiscountPolicy discountPolicy = bean.getDiscountPolicy();
+        System.out.println("discountPolicy : " + discountPolicy);
     }
 }
