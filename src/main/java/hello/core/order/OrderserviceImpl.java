bbed382cc7106cc3144dbe7d7208af2a4c9cd849
HAN32-NOTEBOOK\han32 <mii2425@naver.com>
2024-07-09 18:55:37 +0900

    6-3 의존관계 자동주입 - 롬복과 최신트렌드

--- src/main/java/hello/core/order/OrderserviceImpl.java
+++ src/main/java/hello/core/order/OrderserviceImpl.java
@@ -6,28 +6,30 @@ import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderserviceImpl implements OrderService{
 
     //    추상클래스(인터페이스)에만 의존하도록 변경
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy; // final 선언되어있으면 기본할당이든 생성자인든 할당이되어야함!
 
-    @Autowired
-    public OrderserviceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+//    @Autowired
+//    public OrderserviceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//    this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         // 1. 회원정보 조회 후
-        Member member = memberRepository.findById(memberId);
+    Member member = memberRepository.findById(memberId);
         // 2. 할인가격 할인정책에 넘김
-        int discountPrice = discountPolicy.discount(member, itemPrice);
+    int discountPrice = discountPolicy.discount(member, itemPrice);
 
         return new Order(memberId, itemName, itemPrice, discountPrice);
     }
