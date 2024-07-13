80374080676d84566f7b5799ed18c1818bb7bf77
HAN32-NOTEBOOK\han32 <mii2425@naver.com>
2024-07-13 21:35:51 +0900

    7-2 빈 생명주기 콜백1 - 인터페이스

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,11 +1,14 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+// InitializingBean(초기화 빈) 인터페이스 상속
+// DisposableBean
+public class NetworkClient implements InitializingBean, DisposableBean {
     public String url;
     public NetworkClient() {
         System.out.println("생성자 호출, url : " + url);
-        connect();
-        call("초기화 연결 메세지");
     }
 
     public void setUrl(String url) {
@@ -20,5 +23,21 @@ public class NetworkClient {
     }
     public void call(String message){
         System.out.println("call url : " + url + " message : " + message);
+    }
+
+    // 의존관계 주입이 끝나면 실행하는 메서드
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("# afterPropertiesSet start ");
+        connect();
+        call("초기화 연결메세지");
+        System.out.println("# afterPropertiesSet end ");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("# destroy start");
+        disconnect();
+        System.out.println("# destroy end ");
     }
 }
